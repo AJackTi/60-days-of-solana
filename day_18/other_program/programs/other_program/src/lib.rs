f4b875457a6a827f598d0bd7006be8aba6a06a49
trong.dang <trong.dang@goopay.vn>
2025-01-30 20:22:15 +0700

    fixed: retest other_program of day_18

--- day_18/other_program/programs/other_program/src/lib.rs
+++ day_18/other_program/programs/other_program/src/lib.rs
@@ -1,13 +1,13 @@
 use anchor_lang::prelude::*;
 use std::mem::size_of;
 
-declare_id!("EW6R34TciFADwYMyWNeq5fQcpYsTd22Sf5GJKGES7Hnr");
+declare_id!("Dn3jP8qvtLdXuqtrrskLhhaaU7Encc1BhjUSPUCDADsQ");
 
 #[program]
 pub mod other_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
@@ -24,14 +24,14 @@ pub struct Initialize<'info> {
 
     system_program: Program<'info, System>,
 
-    #[account(init, payer = signer, space = size_of::<TrueOrFalse>() + 8, seeds=[], bump)]
+    #[account(init, payer = signer, space = size_of::<TrueOrFalse>() + 8, seeds = [], bump)]
     true_or_false: Account<'info, TrueOrFalse>,
 }
 
 #[derive(Accounts)]
 pub struct SetFlag<'info> {
     #[account(mut)]
-    true_or_false: Account<'info, TrueOrFalse>, 
+    true_or_false: Account<'info, TrueOrFalse>,
 }
 
 #[account]
--- day_18/other_program/tests/other_program.ts
+++ day_18/other_program/tests/other_program.ts
@@ -1,6 +1,7 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { OtherProgram } from "../target/types/other_program";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+
+import { OtherProgram } from '../target/types/other_program';
 
 describe("other_program", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -8,12 +9,24 @@ describe("other_program", () => {
   const program = anchor.workspace.OtherProgram as Program<OtherProgram>;
 
   it("Is initialized!", async () => {
-    const seeds = []
-    const [TrueOrFalse, _bump] = anchor.web3.PublicKey.findProgramAddressSync(seeds, program.programId);
+    const seeds = [];
+    const [trueOrFalse, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
 
     console.log("address: ", program.programId.toBase58());
 
-    await program.methods.initialize().accounts({ trueOrFalse: TrueOrFalse }).rpc();
-    await program.methods.setbool(true).accounts({ trueOrFalse: TrueOrFalse }).rpc();
+    await program.methods
+      .initialize()
+      .accounts({ trueOrFalse: trueOrFalse })
+      .rpc();
+    await program.methods
+      .setbool(true)
+      .accounts({ trueOrFalse: trueOrFalse })
+      .rpc();
+
+    let myStorageStruct = await program.account.trueOrFalse.fetch(trueOrFalse);
+    console.log("The value of flag is:", myStorageStruct.flag.toString());
   });
 });

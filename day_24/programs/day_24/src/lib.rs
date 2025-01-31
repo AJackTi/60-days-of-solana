af6b84451f876913dd42afe99edb1b9afcb7d18a
trong.dang <trong.dang@goopay.vn>
2025-02-01 00:32:44 +0700

    fixed: retest day_24

--- day_24/programs/day_24/src/lib.rs
+++ day_24/programs/day_24/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use std::mem::size_of;
 
-declare_id!("Ec5TujfQgp2ZTxtPafgN4LeMMHD9HMmpYczqyHrdocLF");
+declare_id!("ApLXPPbSVSvEkkss2pzyKKpXFxXj6uuVN7Bsu3Y4XsBx");
 
 const STARTING_POINTS: u32 = 10;
 
@@ -9,8 +9,8 @@ const STARTING_POINTS: u32 = 10;
 pub mod day_24 {
     use super::*;
 
-    // for update_value example
-    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 
@@ -19,51 +19,34 @@ pub mod day_24 {
         Ok(())
     }
 
-    // For transfer_points example
     pub fn initialize2(ctx: Context<Initialize2>) -> Result<()> {
         ctx.accounts.player.points = STARTING_POINTS;
         ctx.accounts.player.authority = ctx.accounts.signer.key();
         Ok(())
     }
 
-    pub fn transfer_points(ctx: Context<TransferPoints>,
-                          amount: u32) -> Result<()> {
-        // In the final example, we use `has_one` and `constraint` constraints as an alternative to `require!`.
-        // require!(ctx.accounts.from.authority == ctx.accounts.signer.key(),
-        //         Errors::SignerIsNotAuthority);
-        // require!(ctx.accounts.from.points >= amount,
-        //         Errors::InsufficientPoints);
-        
+    pub fn transfer_points(ctx: Context<TransferPoints>, amount: u32) -> Result<()> {
+        require!(
+            ctx.accounts.from.authority == ctx.accounts.signer.key(),
+            Errors::SignerIsNotAuthority
+        );
+        require!(ctx.accounts.from.points >= amount, Errors::InsufficientPoints);
+
         ctx.accounts.from.points -= amount;
         ctx.accounts.to.points += amount;
         Ok(())
     }
 }
 
-// Update Value Example
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init,
-              payer = fren,
-              space=size_of::<MyStorage>() + 8,
-              seeds = [],
-              bump)]
+    #[account(init, payer = signer, space = size_of::<MyStorage>() + 8, seeds = [], bump)]
     pub my_storage: Account<'info, MyStorage>,
 
     #[account(mut)]
-    pub fren: Signer<'info>,
+    pub signer: Signer<'info>,
 
     pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct UpdateValue<'info> {
-    #[account(mut, seeds = [], bump)]
-    pub my_storage: Account<'info, MyStorage>,
-
-	// THIS FIELD MUST BE INCLUDED
-    #[account(mut)]
-    pub fren: Signer<'info>,
 }
 
 #[account]
@@ -71,43 +54,55 @@ pub struct MyStorage {
     x: u64,
 }
 
-// Transfer Points Example
+#[derive(Accounts)]
+pub struct UpdateValue<'info> {
+    #[account(mut, seeds = [], bump)]
+    pub my_storage: Account<'info, MyStorage>,
+
+    // THIS FIELD MUST BE INCLUDED
+    #[account(signer)]
+    pub signer: Signer<'info>,
+}
+
 #[error_code]
 pub enum Errors {
     #[msg("SignerIsNotAuthority")]
     SignerIsNotAuthority,
     #[msg("InsufficientPoints")]
-    InsufficientPoints
+    InsufficientPoints,
 }
 
 #[derive(Accounts)]
 pub struct Initialize2<'info> {
-    #[account(init,
-              payer = signer,
-              space = size_of::<Player>() + 8,
-              seeds = [&(signer.as_ref().key().to_bytes())],
-              bump)]
+    #[account(
+        init,
+        payer = signer,
+        space = size_of::<Player>() + 8,
+        seeds = [&signer.as_ref().key().to_bytes()],
+        bump
+    )]
     player: Account<'info, Player>,
+
     #[account(mut)]
     signer: Signer<'info>,
+
     system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(amount: u32)] // amount must be passed as an instruction
 pub struct TransferPoints<'info> {
-    #[account(mut,
-              has_one = authority @ Errors::SignerIsNotAuthority,
-              constraint = from.points >= amount @ Errors::InsufficientPoints)]
+    #[account(mut)]
     from: Account<'info, Player>,
+
     #[account(mut)]
     to: Account<'info, Player>,
+
     #[account(mut)]
-    authority: Signer<'info>,
+    signer: Signer<'info>,
 }
 
 #[account]
 pub struct Player {
     points: u32,
-    authority: Pubkey
+    authority: Pubkey,
 }
--- day_24/tests/day_24.ts
+++ day_24/tests/day_24.ts
@@ -1,23 +1,27 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Day24 } from "../target/types/day_24";
-import chai from "chai";
-import chaiAsPromised from "chai-as-promised";
+import chai from 'chai';
 
-chai.use(chaiAsPromised);
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+
+import { Day24 } from '../target/types/day_24';
+
 const expect = chai.expect;
 
 // this airdrops sol to an address
 async function airdropSol(publicKey, amount) {
-  let airdropTx = await anchor.getProvider().connection.requestAirdrop(publicKey, amount * anchor.web3.LAMPORTS_PER_SOL);
+  let airdropTx = await anchor
+    .getProvider()
+    .connection.requestAirdrop(publicKey, amount);
   await confirmTransaction(airdropTx);
 }
 
 async function confirmTransaction(tx) {
-  const latestBlockHash = await anchor.getProvider().connection.getLatestBlockhash();
+  const latestBlockhash = await anchor
+    .getProvider()
+    .connection.getLatestBlockhash();
   await anchor.getProvider().connection.confirmTransaction({
-    blockhash: latestBlockHash.blockhash,
-    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    blockhash: latestBlockhash.blockhash,
+    lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
     signature: tx,
   });
 }
@@ -28,159 +32,272 @@ describe("day_24", () => {
 
   const program = anchor.workspace.Day24 as Program<Day24>;
 
-  it("should update value", async () => {
-    const alice = anchor.web3.Keypair.generate();
-    const bob = anchor.web3.Keypair.generate();
-
-    await airdropSol(alice.publicKey, 1); // 1 SOL
-    await airdropSol(bob.publicKey, 1); // 1 SOL
+  it("update value", async () => {
+    /*
+    const newKeypair = anchor.web3.Keypair.generate();
+    await airdropSol(newKeypair.publicKey, 1e9); // 1 SOL
 
     let seeds = [];
-    const [myStorage, _bump] = anchor.web3.PublicKey.findProgramAddressSync(seeds, program.programId);
+    const [myStorage, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
 
-    // ALICE INITIALIZE ACCOUNT
-    await program.methods.initialize().accounts({
-      myStorage: myStorage,
-      fren: alice.publicKey
-    }).signers([alice]).rpc();
+    await program.methods
+      .initialize()
+      .accounts({
+        myStorage,
+        signer: newKeypair.publicKey, // ** THIS MUST BE THE EXPLICITLY SPECIFIED **
+      })
+      .signers([newKeypair])
+      .rpc();
+    */
+    // OR
+    /*
+      await program.methods
+        .initialize()
+        .accounts({
+          myStorage,
+        })
+        .rpc();
+    */
 
-    // BOB WRITE TO ACCOUNT
-    await program.methods.updateValue(new anchor.BN(3)).accounts({
-      myStorage: myStorage,
-      fren: bob.publicKey
-    }).signers([bob]).rpc();
-
-    let value = await program.account.myStorage.fetch(myStorage);
-    console.log(`value stored is ${value.x}`);
-  });
-
-  it("should transfer points from Alice to Bob", async () => {
     const alice = anchor.web3.Keypair.generate();
     const bob = anchor.web3.Keypair.generate();
 
-    await airdropSol(alice.publicKey, 1); // 1 SOL
-    await airdropSol(bob.publicKey, 1); // 1 SOL
+    const airdropAliceTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        alice.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      );
+    await confirmTransaction(airdropAliceTx);
 
-    let seeds_alice = [alice.publicKey.toBytes()];
-    const [playerAlice, _bumpA] = anchor.web3.PublicKey.findProgramAddressSync(seeds_alice, program.programId);
+    const airdropBobTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        bob.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      );
+    await confirmTransaction(airdropBobTx);
 
-    let seeds_bob = [bob.publicKey.toBytes()];
-    const [playerBob, _bumpB] = anchor.web3.PublicKey.findProgramAddressSync(seeds_bob, program.programId);
+    let seeds = [];
+    const [myStorage, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
+
+    // ALICE INITIALIZE ACCOUNT
+    await program.methods
+      .initialize()
+      .accounts({
+        myStorage,
+        signer: alice.publicKey,
+      })
+      .signers([alice])
+      .rpc();
+
+    // BOB WRITE TO ACCOUNT
+    await program.methods
+      .updateValue(new anchor.BN(3))
+      .accounts({
+        myStorage,
+        signer: bob.publicKey,
+      })
+      .signers([bob])
+      .rpc();
+
+    let value = await program.account.myStorage.fetch(myStorage);
+    console.log(`value stored is: ${value.x}`);
+  });
+
+  it("points", async () => {
+    const alice = anchor.web3.Keypair.generate();
+    const bob = anchor.web3.Keypair.generate();
+
+    const airdropAliceTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        alice.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      );
+    await confirmTransaction(airdropAliceTx);
+
+    const airdropBobTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        bob.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      );
+    await confirmTransaction(airdropBobTx);
+
+    let seedsAlice = [alice.publicKey.toBytes()];
+    const [playerAlice, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      seedsAlice,
+      program.programId
+    );
+
+    let seedsBob = [bob.publicKey.toBytes()];
+    const [playerBob, _bump2] = anchor.web3.PublicKey.findProgramAddressSync(
+      seedsBob,
+      program.programId
+    );
 
     // Alice and Bob initialize their accounts
-    await program.methods.initialize2().accounts({
-      player: playerAlice,
-      signer: alice.publicKey,
-    }).signers([alice]).rpc();
+    await program.methods
+      .initialize2()
+      .accounts({
+        player: playerAlice,
+        signer: alice.publicKey,
+      })
+      .signers([alice])
+      .rpc();
 
-    await program.methods.initialize2().accounts({
-      player: playerBob,
-      signer: bob.publicKey,
-    }).signers([bob]).rpc();
+    await program.methods
+      .initialize2()
+      .accounts({
+        player: playerBob,
+        signer: bob.publicKey,
+      })
+      .signers([bob])
+      .rpc();
 
     // Alice transfers 5 points to Bob. Note that this is a u32
     // so we don't need a BigNum
-    await program.methods.transferPoints(5).accounts({
-      from: playerAlice,
-      to: playerBob,
-      authority: alice.publicKey,
-    }).signers([alice]).rpc();
+    await program.methods
+      .transferPoints(5)
+      .accounts({
+        from: playerAlice,
+        to: playerBob,
+        signer: alice.publicKey,
+      })
+      .signers([alice])
+      .rpc();
 
-    console.log(`Alice has ${(await program.account.player.fetch(playerAlice)).points} points`);
-    console.log(`Bob has ${(await program.account.player.fetch(playerBob)).points} points`)
+    console.log(
+      `Alice has ${
+        (await program.account.player.fetch(playerAlice)).points
+      } points`
+    );
+    console.log(
+      `Bob has ${(await program.account.player.fetch(playerBob)).points} points`
+    );
   });
 
-  it("should prevent Alice from transferring more points than her balance", async () => {
-    const alice = anchor.web3.Keypair.generate();
-    const bob = anchor.web3.Keypair.generate();
-
-    await airdropSol(alice.publicKey, 1); // 1 SOL
-    await airdropSol(bob.publicKey, 1); // 1 SOL
-
-    let seeds_alice = [alice.publicKey.toBytes()];
-    const [playerAlice, _bumpA] = anchor.web3.PublicKey.findProgramAddressSync(seeds_alice, program.programId);
-
-    let seeds_bob = [bob.publicKey.toBytes()];
-    const [playerBob, _bumpB] = anchor.web3.PublicKey.findProgramAddressSync(seeds_bob, program.programId);
-
-    // Alice and Bob initialize their accounts
-    await program.methods.initialize2().accounts({
-      player: playerAlice,
-      signer: alice.publicKey,
-    }).signers([alice]).rpc();
-
-    await program.methods.initialize2().accounts({
-      player: playerBob,
-      signer: bob.publicKey,
-    }).signers([bob]).rpc();
-
-    // Alice transfers 15 points to Bob, which exceeds her points balance of 10
-    const transferToBob = program.methods.transferPoints(15).accounts({
-      from: playerAlice,
-      to: playerBob,
-      authority: alice.publicKey,
-    }).signers([alice]).rpc();
-
-    await expect(transferToBob).to.be.eventually.rejected;
-
-    console.log(`Alice has ${(await program.account.player.fetch(playerAlice)).points} points`);
-    console.log(`Bob has ${(await program.account.player.fetch(playerBob)).points} points`)
-  });
-
-  it("should prevent Mallory from stealing points", async () => {
+  it("mallory steal points", async () => {
     const alice = anchor.web3.Keypair.generate();
     const bob = anchor.web3.Keypair.generate();
     const mallory = anchor.web3.Keypair.generate();
 
-    await airdropSol(alice.publicKey, 1); // 1 SOL
-    await airdropSol(bob.publicKey, 1); // 1 SOL
-    await airdropSol(mallory.publicKey, 1); // 1 SOL
+    const airdropAliceTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        alice.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      );
+    await confirmTransaction(airdropAliceTx);
 
-    let seeds_alice = [alice.publicKey.toBytes()];
-    const [playerAlice, _bumpA] = anchor.web3.PublicKey.findProgramAddressSync(seeds_alice, program.programId);
+    const airdropBobTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        bob.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      );
+    await confirmTransaction(airdropBobTx);
 
-    let seeds_bob = [bob.publicKey.toBytes()];
-    const [playerBob, _bumpB] = anchor.web3.PublicKey.findProgramAddressSync(seeds_bob, program.programId);
+    const airdropMalloryTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        mallory.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      );
+    await confirmTransaction(airdropMalloryTx);
 
-    let seeds_mallory = [mallory.publicKey.toBytes()];
-    const [playerMallory, _bumpC] = anchor.web3.PublicKey.findProgramAddressSync(seeds_mallory, program.programId);
+    let seedsAlice = [alice.publicKey.toBytes()];
+    const [playerAlice, _bumpA] = anchor.web3.PublicKey.findProgramAddressSync(
+      seedsAlice,
+      program.programId
+    );
+
+    let seedsBob = [bob.publicKey.toBytes()];
+    const [playerBob, _bumpB] = anchor.web3.PublicKey.findProgramAddressSync(
+      seedsBob,
+      program.programId
+    );
+
+    let seedsMallory = [mallory.publicKey.toBytes()];
+    const [playerMallory, _bumpC] =
+      anchor.web3.PublicKey.findProgramAddressSync(
+        seedsMallory,
+        program.programId
+      );
 
     // Alice, Bob, and Mallory initialize their accounts
-    await program.methods.initialize2().accounts({
-      player: playerAlice,
-      signer: alice.publicKey,
-    }).signers([alice]).rpc();
+    await program.methods
+      .initialize2()
+      .accounts({
+        player: playerAlice,
+        signer: alice.publicKey,
+      })
+      .signers([alice])
+      .rpc();
 
-    await program.methods.initialize2().accounts({
-      player: playerBob,
-      signer: bob.publicKey,
-    }).signers([bob]).rpc();
+    await program.methods
+      .initialize2()
+      .accounts({
+        player: playerBob,
+        signer: bob.publicKey,
+      })
+      .signers([bob])
+      .rpc();
 
-    await program.methods.initialize2().accounts({
-      player: playerMallory,
-      signer: mallory.publicKey,
-    }).signers([mallory]).rpc();
+    await program.methods
+      .initialize2()
+      .accounts({
+        player: playerMallory,
+        signer: mallory.publicKey,
+      })
+      .signers([mallory])
+      .rpc();
 
     // This will throw an error
-    const stealFromAlice = program.methods.transferPoints(5).accounts({
-      from: playerAlice,
-      to: playerMallory,
-      authority: mallory.publicKey,
-    }).signers([mallory]).rpc();
+    const stealFromAlice = program.methods
+      .transferPoints(5)
+      .accounts({
+        from: playerAlice,
+        to: playerMallory,
+        authority: mallory.publicKey,
+      })
+      .signers([mallory])
+      .rpc();
 
-    await expect(stealFromAlice).to.be.eventually.rejected;
+    await expect(stealFromAlice).to.be.Throw;
 
     // This will also throw an error
-    const stealFromBob = program.methods.transferPoints(5).accounts({
-      from: playerBob,
-      to: playerMallory,
-      authority: mallory.publicKey,
-    }).signers([mallory]).rpc();
+    const stealFromBob = program.methods
+      .transferPoints(5)
+      .accounts({
+        from: playerBob,
+        to: playerMallory,
+        authority: mallory.publicKey,
+      })
+      .signers([mallory])
+      .rpc();
 
-    await expect(stealFromBob).to.be.eventually.rejected;
+    await expect(stealFromBob).to.be.Throw;
 
-    console.log(`Alice has ${(await program.account.player.fetch(playerAlice)).points} points`);
-    console.log(`Bob has ${(await program.account.player.fetch(playerBob)).points} points`)
-    console.log(`Mallory has ${(await program.account.player.fetch(playerMallory)).points} points`)
+    console.log(
+      `Alice has ${
+        (await program.account.player.fetch(playerAlice)).points
+      } points`
+    );
+    console.log(
+      `Bob has ${(await program.account.player.fetch(playerBob)).points} points`
+    );
+    console.log(
+      `Mallory has ${
+        (await program.account.player.fetch(playerMallory)).points
+      } points`
+    );
   });
 });

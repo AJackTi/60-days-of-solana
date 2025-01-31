a773079b0d7ab0b3cc068f2f5d09ea845a151a51
trong.dang <trong.dang@goopay.vn>
2025-01-31 14:07:05 +0700

    fixed: update day_19

--- day_19/programs/day_19/src/lib.rs
+++ day_19/programs/day_19/src/lib.rs
@@ -1,120 +1,186 @@
 use anchor_lang::prelude::*;
 use std::mem::size_of;
 
-declare_id!("rRUYYHFzoCGRpKWMX3oiQsm9XCC5iuSDCNAFye8Ann2");
+declare_id!("orMJEWHq2uxYZfzRvyZF7cbkGKnQdfjSza4Ej3cEr5S");
 
 #[program]
 pub mod day_19 {
     use super::*;
 
-    // Mapping
-    pub fn initialize_mapping(_ctx: Context<InitializeMapping>, _key: u64) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>, key: u64) -> Result<()> {
         Ok(())
     }
 
-    pub fn set_mapping(ctx: Context<SetMapping>, _key: u64, val: u64) -> Result<()> {
+    pub fn set(ctx: Context<Set>, key: u64, val: u64) -> Result<()> {
         ctx.accounts.val.value = val;
         Ok(())
     }
 
-    // Nested Mapping
-    pub fn initialize_nested_mapping(_ctx: Context<InitializeNestedMapping>, _key1: u64, _key2: u64, _key3: u64) -> Result<()> {
+    pub fn initialize_nested_mapping(
+        _ctx: Context<InitializeNestedMapping>,
+        key1: u64,
+        key2: u64
+    ) -> Result<()> {
         Ok(())
     }
 
-    pub fn set_nested_mapping(ctx: Context<SetNestedMapping>, _key1: u64, _key2: u64, _key3: u64, val: u64) -> Result<()> {
+    pub fn set_nested_mapping(
+        ctx: Context<SetNestedMapping>,
+        key1: u64,
+        key2: u64,
+        val: u64
+    ) -> Result<()> {
         ctx.accounts.val.value = val;
         Ok(())
     }
 
-    // // Multiple Mappings
-    // pub fn initialize_multiple_mappings(_ctx: Context<InitializeMultipleMappings>, _which_map: u64, _key1: u64, _key2: u64) -> Result<()> {
-    //     Ok(())
-    // }
+    pub fn initialize_map1(ctx: Context<InitializeMap1>, which_map: u64, key1: u64) -> Result<()> {
+        ctx.accounts.val1.value = 0;
+        Ok(())
+    }
 
-    // pub fn set_multiple_mappings(ctx: Context<SetNestedMapping>, _which_map: u64, _key1: u64, _key2: u64, val: u64) -> Result<()> {
-    //     ctx.accounts.val.value = val;
-    //     Ok(())
-    // }
+    pub fn set_map1(ctx: Context<SetMap1>, which_map: u64, key1: u64, val: u64) -> Result<()> {
+        ctx.accounts.val.value = val;
+        Ok(())
+    }
+
+    pub fn initialize_map2(
+        ctx: Context<InitializeMap2>,
+        which_map: u64,
+        key1: u64,
+        key2: u64
+    ) -> Result<()> {
+        ctx.accounts.val2.value = 0;
+        Ok(())
+    }
+
+    pub fn set_map2(
+        ctx: Context<SetMap2>,
+        which_map: u64,
+        key1: u64,
+        key2: u64,
+        val: u64
+    ) -> Result<()> {
+        ctx.accounts.val.value = val;
+        Ok(())
+    }
 }
 
-// Mapping
 #[derive(Accounts)]
 #[instruction(key: u64)]
-pub struct InitializeMapping<'info> {
-
-    #[account(init,
-              payer = signer,
-              space = size_of::<Val>() + 8,
-              seeds=[&key.to_le_bytes().as_ref()],
-              bump)]
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = size_of::<Val>() + 8,
+        seeds = [&key.to_le_bytes().as_ref()],
+        bump
+    )]
     val: Account<'info, Val>,
-    
+
     #[account(mut)]
     signer: Signer<'info>,
-    
+
     system_program: Program<'info, System>,
 }
 
+#[account]
+pub struct Val {
+    value: u64,
+}
+
 #[derive(Accounts)]
 #[instruction(key: u64)]
-pub struct SetMapping<'info> {
+pub struct Set<'info> {
     #[account(mut)]
     val: Account<'info, Val>,
 }
 
-// Nested Mapping
 #[derive(Accounts)]
-#[instruction(key1: u64, key2: u64, key3: u64)]
+#[instruction(key1: u64, key2: u64)]
 pub struct InitializeNestedMapping<'info> {
-
-    #[account(init,
-              payer = signer,
-              space = size_of::<Val>() + 8,
-              seeds=[&key1.to_le_bytes().as_ref(), &key2.to_le_bytes().as_ref(), &key3.to_le_bytes().as_ref()],
-              bump)]
+    #[account(
+        init,
+        payer = signer,
+        space = size_of::<Val>() + 8,
+        seeds = [&key1.to_le_bytes().as_ref(), &key2.to_le_bytes().as_ref()],
+        bump
+    )]
     val: Account<'info, Val>,
-    
+
     #[account(mut)]
     signer: Signer<'info>,
-    
+
     system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-#[instruction(key1: u64, key2: u64, key3: u64)]
+#[instruction(key1: u64, key2: u64)]
 pub struct SetNestedMapping<'info> {
     #[account(mut)]
     val: Account<'info, Val>,
 }
 
-// // Multiple Mappings
-// #[derive(Accounts)]
-// #[instruction(which_map: u64, key1: u64, key2: u64)]
-// pub struct InitializeMultipleMappings<'info> {
+#[account]
+pub struct Val1 {
+    value: u64,
+}
 
-//     #[account(init,
-//               payer = signer,
-//               space = size_of::<Val>() + 8,
-//               seeds=[&which_map.to_le_bytes().as_ref(), &key1.to_le_bytes().as_ref(), &key2.to_le_bytes().as_ref()],
-//               bump)]
-//     val: Account<'info, Val>,
-    
-//     #[account(mut)]
-//     signer: Signer<'info>,
-    
-//     system_program: Program<'info, System>,
-// }
+#[account]
+pub struct Val2 {
+    value: u64,
+}
+
+#[derive(Accounts)]
+#[instruction(which_map: u64, key1: u64)]
+pub struct InitializeMap1<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = size_of::<Val1>() + 8,
+        seeds = [&which_map.to_le_bytes().as_ref(), &key1.to_le_bytes().as_ref()],
+        bump
+    )]
+    val1: Account<'info, Val1>,
+
+    #[account(mut)]
+    signer: Signer<'info>,
+
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(which_map: u64, key1: u64)]
+pub struct SetMap1<'info> {
+    #[account(mut)]
+    val: Account<'info, Val1>,
+}
 
 #[derive(Accounts)]
 #[instruction(which_map: u64, key1: u64, key2: u64)]
-pub struct SetMultipleMappings<'info> {
+pub struct InitializeMap2<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = size_of::<Val2>() + 8,
+        seeds = [
+            &which_map.to_le_bytes().as_ref(),
+            &key1.to_le_bytes().as_ref(),
+            &key2.to_le_bytes().as_ref(),
+        ],
+        bump
+    )]
+    pub val2: Account<'info, Val2>,
+
     #[account(mut)]
-    val: Account<'info, Val>,
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
 }
 
-
-#[account]
-pub struct Val {
-    value: u64,
+#[derive(Accounts)]
+#[instruction(which_map: u64, key1: u64, key2: u64)]
+pub struct SetMap2<'info> {
+    #[account(mut)]
+    val: Account<'info, Val2>,
 }
--- day_19/tests/day_19.ts
+++ day_19/tests/day_19.ts
@@ -1,6 +1,7 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Day19 } from "../target/types/day_19";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+
+import { Day19 } from '../target/types/day_19';
 
 describe("day_19", () => {
   // Configure the client to use the local cluster.
@@ -8,81 +9,105 @@ describe("day_19", () => {
 
   const program = anchor.workspace.Day19 as Program<Day19>;
 
-  it("Initialize and set mapping value", async () => {
+  it("Initialize mapping storage", async () => {
     const key = new anchor.BN(42);
     const value = new anchor.BN(1337);
-
     const seeds = [key.toArrayLike(Buffer, "le", 8)];
+
     let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
       seeds,
-      program.programId,
+      program.programId
     )[0];
 
-    await program.methods.initializeMapping(key).accounts({ val: valueAccount }).rpc();
-    await program.methods.setMapping(key, value).accounts({ val: valueAccount }).rpc();
+    await program.methods.initialize(key).accounts({ val: valueAccount }).rpc();
+
+    // set the account
+    await program.methods.set(key, value).accounts({ val: valueAccount }).rpc();
 
     // read the account back
     let result = await program.account.val.fetch(valueAccount);
-    console.log(`the value ${result.value} was stored in ${valueAccount.toBase58()}`);
+
+    console.log(
+      `the value ${result.value} was stored in ${valueAccount.toBase58()}`
+    );
   });
 
-  it("Initialize and set nested mapping value", async () => {
+  it("Initialize nested mapping storage", async () => {
     const key1 = new anchor.BN(42);
     const key2 = new anchor.BN(43);
-    const key3 = new anchor.BN(44);
     const value = new anchor.BN(1337);
 
+    // seeds has two values
     const seeds = [
       key1.toArrayLike(Buffer, "le", 8),
       key2.toArrayLike(Buffer, "le", 8),
-      key3.toArrayLike(Buffer, "le", 8)
     ];
     let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
       seeds,
-      program.programId,
+      program.programId
     )[0];
 
-    await program.methods.initializeNestedMapping(key1, key2, key3).accounts({ val: valueAccount }).rpc();
-    await program.methods.setNestedMapping(key1, key2, key3, value).accounts({ val: valueAccount }).rpc();
+    // functions now take two keys
+    await program.methods
+      .initializeNestedMapping(key1, key2)
+      .accounts({ val: valueAccount })
+      .rpc();
+
+    await program.methods
+      .setNestedMapping(key1, key2, value)
+      .accounts({ val: valueAccount })
+      .rpc();
 
     // read the account back
     let result = await program.account.val.fetch(valueAccount);
-    console.log(`the value ${result.value} was stored in ${valueAccount.toBase58()}`);
-
+    console.log(
+      `the value ${result.value} was stored in ${valueAccount.toBase58()}`
+    );
   });
 
-  // it("Initialize and set multiple mapping values", async () => {
-  //   const whichMap1 = new anchor.BN(1);
-  //   const whichMap2 = new anchor.BN(2);
-  //   const key1 = new anchor.BN(42);
-  //   const key2 = new anchor.BN(43);
-  //   const value1 = new anchor.BN(1337);
-  //   const value2 = new anchor.BN(420);
+  it("Initialize Map 1 (Single Key)", async () => {
+    const whichMap = new anchor.BN(0);
+    const key1 = new anchor.BN(42);
 
-  //   const seeds = [
-  //     whichMap1.toArrayLike(Buffer, "le", 8),
-  //     key1.toArrayLike(Buffer, "le", 8),
-  //     key2.toArrayLike(Buffer, "le", 8),
-  //   ];
-  //   let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
-  //     seeds,
-  //     program.programId,
-  //   )[0];
+    const seeds = [
+      whichMap.toArrayLike(Buffer, "le", 8),
+      key1.toArrayLike(Buffer, "le", 8),
+    ];
 
-  //   await program.methods.initializeMultipleMappings(whichMap1, key1, key2).accounts({ val: valueAccount }).rpc();
-  //   await program.methods.setMultipleMappings(whichMap1, key1, key2, value1).accounts({ val: valueAccount }).rpc();
+    const [val1Account, _] = await anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
 
-  //   // read the account back
-  //   let result1 = await program.account.val.fetch(valueAccount);
-  //   console.log(`the value ${result1.value} was stored in ${valueAccount.toBase58()}`);
+    await program.methods
+      .initializeMap1(whichMap, key1)
+      .accounts({
+        val1: val1Account,
+      })
+      .rpc();
+  });
 
+  it("Initialize Map 2 (Two Keys)", async () => {
+    const whichMap = new anchor.BN(1);
+    const key1 = new anchor.BN(42);
+    const key2 = new anchor.BN(100);
 
-  //   await program.methods.initializeMultipleMappings(whichMap2, key1, key2).accounts({ val: valueAccount }).rpc();
-  //   await program.methods.setMultipleMappings(whichMap2, key1, key2, value2).accounts({ val: valueAccount }).rpc();
+    const seeds = [
+      whichMap.toArrayLike(Buffer, "le", 8),
+      key1.toArrayLike(Buffer, "le", 8),
+      key2.toArrayLike(Buffer, "le", 8),
+    ];
 
-  //   // read the account back
-  //   let result = await program.account.val.fetch(valueAccount);
-  //   console.log(`the value ${result.value} was stored in ${valueAccount.toBase58()}`);
+    const [val2Account, _] = await anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
 
-  // });
+    await program.methods
+      .initializeMap2(whichMap, key1, key2)
+      .accounts({
+        val2: val2Account,
+      })
+      .rpc();
+  });
 });

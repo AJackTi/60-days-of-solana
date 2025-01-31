c69c8420776aeae377a647b0545e61963fe36c0b
trong.dang <trong.dang@goopay.vn>
2025-01-31 17:56:34 +0700

    fixed: retest day_21

--- day_21/programs/day_21/src/lib.rs
+++ day_21/programs/day_21/src/lib.rs
@@ -1,21 +1,29 @@
 use anchor_lang::prelude::*;
 
-declare_id!("14daYtTrYmn6KfkGYeWMKNJ6iQEZD3eLUozCzjL13ucS");
+declare_id!("6mwNiZ4Coxy1ir4aXyhcfG7g7bCaaKFsXPY4UACExPUp");
 
 #[program]
 pub mod day_21 {
     use super::*;
 
-    pub fn read_balance(ctx: Context<ReadBalance>) -> Result<()> {
-        let balance = ctx.accounts.acct.to_account_info().lamports();
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
 
-        msg!("balance in Lamports is {}", balance);
+    pub fn read_balance(ctx: Context<ReadBalance>) -> Result<()> {
+        let balance = ctx.accounts.account.to_account_info().lamports();
+
+        msg!("balance in Lamports: {:?}", balance);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
+pub struct Initialize {}
+
+#[derive(Accounts)]
 pub struct ReadBalance<'info> {
     /// CHECK: although we read this account's balance, we don't do anything with the information
-    pub acct: UncheckedAccount<'info>,
+    pub account: UncheckedAccount<'info>,
 }
--- day_21/tests/day_21.ts
+++ day_21/tests/day_21.ts
@@ -1,6 +1,7 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Day21 } from "../target/types/day_21";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+
+import { Day21 } from '../target/types/day_21';
 
 describe("day_21", () => {
   // Configure the client to use the local cluster.
@@ -8,12 +9,21 @@ describe("day_21", () => {
 
   const program = anchor.workspace.Day21 as Program<Day21>;
 
-  // the following is the Solana wallet we are using
-  let pubkey = new anchor.web3.PublicKey("14daYtTrYmn6KfkGYeWMKNJ6iQEZD3eLUozCzjL13ucS");
+  let pubkey = new anchor.web3.PublicKey(
+    "AzuiWapU4pttFt7qQLHaiQMcuhzVb2mwEok5LRWgZJZx"
+  );
 
-  it("reads the account balance", async () => {
+  it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.readBalance().accounts({ acct: pubkey }).rpc();
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+
+  it("Tests the balance", async () => {
+    const tx = await program.methods
+      .readBalance()
+      .accounts({ account: pubkey })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });

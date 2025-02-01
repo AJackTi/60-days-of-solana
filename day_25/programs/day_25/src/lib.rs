ee40cb5d871440c28e97585a9e9abb10a4e6fc19
trong.dang <trong.dang@goopay.vn>
2025-02-01 12:22:41 +0700

    fixed: retest day_25

--- day_25/programs/day_25/src/lib.rs
+++ day_25/programs/day_25/src/lib.rs
@@ -1,30 +1,27 @@
 use anchor_lang::prelude::*;
-use std::mem::size_of; 
+use std::mem::size_of;
 
-declare_id!("F1u71f74aBdLTixkgkTDUVC3Z8ySN8sc8cGERQ2T6DJV");
+declare_id!("Btyg9YJ5z4PY47U9Nz4KnDrWEwCqJzotXCFeKkbA3TWN");
 
 #[program]
 pub mod day_25 {
     use super::*;
 
-    pub fn initialize_pda(_ctx: Context<InitializePDA>) -> Result<()> {
+    pub fn initialize_pda(ctx: Context<InitializePDA>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 
-    pub fn initialize_keypair_account(_ctx: Context<InitializeKeypairAccount>) -> Result<()> {
+    pub fn initialize_keypair_account(ctx: Context<InitializeKeypairAccount>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct InitializePDA<'info> {
-
-		// This is the program derived address
-    #[account(init,
-              payer = signer,
-              space=size_of::<MyPDA>() + 8,
-              seeds = [],
-              bump)]
+    // This is the program derived address
+    #[account(init, payer = signer, space = size_of::<MyPDA>() + 8, seeds = [], bump)]
     pub my_pda: Account<'info, MyPDA>,
 
     #[account(mut)]
@@ -40,10 +37,8 @@ pub struct MyPDA {
 
 #[derive(Accounts)]
 pub struct InitializeKeypairAccount<'info> {
-		// This is the program derived address
-    #[account(init,
-              payer = signer,
-              space = size_of::<MyKeypairAccount>() + 8,)]
+    // This is the program derived address
+    #[account(init, payer = signer, space = size_of::<MyKeypairAccount>() + 8)]
     pub my_keypair_account: Account<'info, MyKeypairAccount>,
 
     #[account(mut)]
